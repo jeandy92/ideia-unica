4fa531d8137291dbcdda83b6f9b47d5e58988664
jeandy92 <jeandymalmeida@gmail.com>
2021-02-11 19:34:27 -0300

    Primeiro Commit

--- pages/components/index.js
+++ pages/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/css/index.module.css';
-import App from './App';
+import App from './pages/App';
 
 function Index(){
 return(<div> <React.StrictMode>
